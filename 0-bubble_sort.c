640e44f82b56ded2ae9b91f142edeb4923a5d11c
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-01-21 08:56:20 +0000

    new commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,9 +21,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				tmp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = tmp;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 				swapped = 1;
 				print_array(array, size);
 			}
