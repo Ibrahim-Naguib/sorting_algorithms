346551a908f1a0c35990533a01840ab94bd6b31a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-01-21 08:45:28 +0000

    new commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers using the Bubble Sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Return: No return value.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, tmp, swapped;
+
+	do {
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	} while (swapped == 1);
+}
